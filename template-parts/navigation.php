fa396ef15e1431f562b8613a0e4a9568ceab3fcc
Albin Metthey <a.metthey@gmail.com>
2020-04-26 10:11:14 +0200

    fix link_link in acf field navigation.php

--- template-parts/navigation.php
+++ template-parts/navigation.php
@@ -11,16 +11,13 @@
 
         <div class="about__parutions">
             <h5>Articles QUD Magazine</h5>        
-            <!--
             <?php if( have_rows('link_container' , 'option') ): ?>
-                <?php while( have_rows('link_container', 'option') ):
-                    $text = get_sub_field('link_text');
-                    $link = get_sub_field('link_link');
-                ?>
-                    <a href="<?php echo $link; ?>"><?php echo $text; ?></a>
+                <?php while( have_rows('link_container', 'option') ): the_row(); ?>
+                    <a href="<?php the_sub_field('link_link'); ?>">
+                        <?php the_sub_field('link_text'); ?>
+                    </a>
                 <?php endwhile; ?>
             <?php endif; ?>
-            -->
 
         </div>
     </div>
