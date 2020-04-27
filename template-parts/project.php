d240136466391e5caf0511b1b5826ceb8ea03438
Albin Metthey <a.metthey@gmail.com>
2020-04-27 19:59:18 +0200

    first step acf ready for project & gallery

--- template-parts/project.php
+++ template-parts/project.php
@@ -1,12 +1,15 @@
 <!-- project -->
 <div class="container__projects" data-simplebar data-simplebar-direction='rtl'> 
-    <!-- https://support.advancedcustomfields.com/forums/topic/unique&#45;id&#45;for&#45;repeater&#45;field/ -->
+    <!-- -->
+    <?php 
+         // https://support.advancedcustomfields.com/forums/topic/unique&#45;id&#45;for&#45;repeater&#45;field/ 
+    ?>
     <?php if( get_field('project_container') ): $i = 0; ?>
         <?php while(has_sub_field('project_container')): $i++; ?>
-            <div id="project-<?php echo $i; ?>" class="project__item">
+            <a href="#project-<?php echo $i; ?>" class="project__item">
                 <h5><?php echo the_sub_field('project_title'); ?></h5>
                 <span><?php echo the_sub_field('project_cat'); ?></span>
-            </div>
+            </a>
         <?php endwhile; ?>
     <?php endif; ?>
 </div>
