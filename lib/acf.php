bef82f2f81bcf9896ce61ee49985f89822dffe8a
Albin Metthey <a.metthey@gmail.com>
2020-04-30 15:01:50 +0200

    1_ resolve hover issue on project (image still here) 2_ reduce arrow size 3_ ACF back office for mobile message 4_ scrollbar always visible

--- lib/acf.php
+++ lib/acf.php
@@ -7,11 +7,23 @@
 if( function_exists('acf_add_options_page') ) {
 
 	acf_add_options_page(array(
-		'page_title' 	=> 'A Propos',
-		'menu_title'	=> 'A Propos',
+		'page_title' 	=> 'Themes Option',
+		'menu_title'	=> 'Themes Option',
 		'menu_slug' 	=> 'theme-general-settings',
 		'capability'	=> 'edit_posts',
 		'redirect'		=> false
+	));
+
+	acf_add_options_sub_page(array(
+		'page_title' 	=> 'Message',
+		'menu_title'	=> 'Message',
+		'parent_slug' 	=> 'theme-general-settings',
+	));
+
+	acf_add_options_sub_page(array(
+		'page_title' 	=> 'A Propos',
+		'menu_title'	=> 'A Propos',
+		'parent_slug' 	=> 'theme-general-settings',
 	));
 
 }
