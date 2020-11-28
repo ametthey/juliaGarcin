953599ffa3e7e0555dc1abba3a92076f036bb59e
Albin Metthey <a.metthey@gmail.com>
2020-11-28 01:16:23 +0100

    udpated

--- template-parts/gallery.php
+++ template-parts/gallery.php
@@ -1,11 +1,10 @@
 
 <?php if( have_rows('gallery_container') ): $i = 0 ?>
 
-<?php while( have_rows('gallery_container') ): the_row(); $i++;
+    <?php while( have_rows('gallery_container') ): the_row(); $i++;
 
         /*
          * Remove p tag before and after Wysiwyg Editor
-         *
          * @see https://support.advancedcustomfields.com/forums/topic/removing-paragraph-tags-from-wysiwyg-fields/
          * @use https://www.advancedcustomfields.com/resources/wysiwyg-editor/
          */
@@ -31,64 +30,69 @@
 
         <?php if( have_rows('gallery_element') ): ?>
 
-        <?php while( have_rows('gallery_element') ): the_row();
+            <?php while( have_rows('gallery_element') ): the_row();
 
-            $image = get_sub_field('gallery_element_image');
-            $embed = get_sub_field('gallery_element_embed');
+                $image = get_sub_field('gallery_element_image');
+                $embed = get_sub_field('gallery_element_embed');
 
-        ?>
-
-        <?php if( $embed ) { ?>
-        <div class="carousel-cell">
-            <div class="vimeo__container">
-            <?php
-
-                // Load value.
-                //$iframe = get_field('oembed');
-
-                // Use preg_match to find iframe src.
-                preg_match('/src="(.+?)"/', $iframe, $matches);
-                $src = $matches[1];
-
-                // Add extra parameters to src and replcae HTML.
-                $params = array(
-                    'controls'  => 0,
-                    'hd'        => 1,
-                    'autohide'  => 1,
-                );
-                $new_src = add_query_arg($params, $src);
-                $iframe = str_replace($src, $new_src, $iframe);
-
-                // Add extra attributes to iframe HTML.
-                $attributes = 'frameborder="0"';
-                $iframe = str_replace('></iframe>', ' ' . $attributes . '></iframe>', $iframe);
-
-                // Display customized HTML.
-                echo $embed;
             ?>
-            </div>
-        </div>
 
-        <?php } else { ?>
+                <?php if( $embed ) { ?>
 
-        <div class="carousel-cell">
-            <!-- <img class="carousel&#45;cell&#45;image" data&#45;flickity&#45;lazyload="<?php echo $image; ?>" alt=""> -->
-            <img src="<?php echo $image; ?>" alt="" class="carousel-cell-image flickity-lazyload">
-        </div>
+                    <!-- Cell: Video -->
+                    <div class="carousel-cell">
+                        <div class="vimeo__container">
+                        <?php
 
-        <?php } ?>
+                            // Load value.
+                            //$iframe = get_field('oembed');
+
+                            // Use preg_match to find iframe src.
+                            preg_match('/src="(.+?)"/', $iframe, $matches);
+                            $src = $matches[1];
+
+                            // Add extra parameters to src and replcae HTML.
+                            $params = array(
+                                'controls'  => 0,
+                                'hd'        => 1,
+                                'autohide'  => 1,
+                            );
+                            $new_src = add_query_arg($params, $src);
+                            $iframe = str_replace($src, $new_src, $iframe);
+
+                            // Add extra attributes to iframe HTML.
+                            $attributes = 'frameborder="0"';
+                            $iframe = str_replace('></iframe>', ' ' . $attributes . '></iframe>', $iframe);
+
+                            // Display customized HTML.
+                            echo $embed;
+                        ?>
+                        </div>
+                    </div>
+
+                <?php } else { ?>
+
+                    <!-- Cell: Photo -->
+                    <div class="carousel-cell">
+                        <img src="<?php echo $image; ?>" alt="" class="carousel-cell-image flickity-lazyload">
+                    </div>
+
+                <?php } ?>
 
 
-        <?php endwhile; ?>
+            <?php endwhile; ?>
 
         <?php endif; ?>
 
 
         </div>
 
-        <div class="flickity-button-wrapper">
-
+        <!-- Close button -->
+        <div class="close-gallery-button">
+            <span>CLOSE</span>
         </div>
+        <!-- <div class="flickity&#45;button&#45;wrapper"> -->
+        <!-- </div> -->
 
     </div>
 </div>
